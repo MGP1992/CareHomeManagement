4f50060abb3ba0d0ec42fd12df4806e04eaa6d79
sonia khan <soniamarei.khan@gmail.com>
2023-05-12 10:28:02 +0100

    yet another merge conflict

--- frontend/src/components/residentProfile/ResidentProfile.jsx
+++ frontend/src/components/residentProfile/ResidentProfile.jsx
@@ -16,28 +16,6 @@ const ResidentProfile = () => {
     });
   }, []);
 
-<<<<<<< HEAD
-
-    return (
-        
-        <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-                <div className="col-md-12 col-xl-4">
-                    <div className="card" style={{"borderRadius": "15px"}}>
-                        <div className="card-body text-center">
-                            <div className="mt-3 mb-4">
-                                <p style={{fontSize: "36px"}}>{residents.firstName} {residents.lastName}</p>
-                                <p style={{fontSize: "24px"}}>{residents.residentID}</p>
-                                <br />
-                                <AddNotes residentID={residents.residentID} />
-                                <div className="activity-notes"><h5>Activities:</h5>{notes.activities}</div><br/>
-                                <div className="medication-notes"><h5>Medications:</h5>{notes.medication}</div><br/>
-                                <div className="wellbeing-notes"><h5>Well-being</h5>{notes.wellbeing}</div><br/>
-                                <div className="other-notes"><h5>Other</h5>{notes.others}</div><br/>
-                            </div>      
-                        </div>
-                    </div>
-=======
   return (
     <div className="container py-5 h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
@@ -54,7 +32,6 @@ const ResidentProfile = () => {
                 <div className="activity-notes">
                   <h5>Activities:</h5>
                   {notes.activities}
->>>>>>> 79dc337a4c5ed26f89aa222c7a49ceb60d6948dd
                 </div>
                 <br />
                 <div className="medication-notes">
